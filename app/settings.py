950256580f1b9713a26d53b9456ec81543d29627
solo <dsogomonian.exceedteam@gmail.com>
2022-06-13 17:51:05 +0300

    nginx conf and settings

--- app/settings.py
+++ app/settings.py
@@ -5,7 +5,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['http://localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
